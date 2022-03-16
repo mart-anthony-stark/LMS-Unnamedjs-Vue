4af72b4f77b956a73aad9f18ebed679262da83bf
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-16 21:07:54 +0800

    initial commit

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const unnamed = require("unnamed-js");
+const dotenv = require("dotenv");
+const { connectDB } = require("./database");
+dotenv.config({});
+
+const server = unnamed({
+  port: process.env.PORT || 8080,
+  init: () => {
+    connectDB(process.env.MONGODB_URI);
+  },
+});
+
+server.combineRouters(require("./routers"));
