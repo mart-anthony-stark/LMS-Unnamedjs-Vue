b00e2ed3730b04302260e52a7e67713e22d2be95
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-16 21:25:07 +0800

    add catcher util

--- server/routers/auth.route.js
+++ server/routers/auth.route.js
@@ -1,5 +1,6 @@
 const UserController = require("../controllers/auth.controller");
+const catcher = require("../utils/catcher.util");
 
 module.exports = ({ GET, POST, PUT, PATCH, DELETE }) => {
-  GET("/", UserController.getAllUsers);
+  GET("/", catcher(UserController.getAllUsers));
 };
