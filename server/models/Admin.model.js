8cfe69373233fae612635420958f911909539499
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-16 21:11:18 +0800

    change to admin

---
+++ server/models/Admin.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const AdminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+});
+
+module.exports = mongoose.model("Admin", AdminSchema);
