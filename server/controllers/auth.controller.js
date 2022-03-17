a81eb65dff1a971ef3285c4aec4fe08149844d4e
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-17 11:26:55 +0800

    test route guard

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const Admin = require("../models/Admin.model");
 
 module.exports = {
   getAllUsers: async (request, response) => {
-    const admins = await Admin.findById("65a4d");
-    response.send(admins);
+    // const admins = await Admin.findById("65a4d");
+    response.send({ admins: "request.admins", admins: request.admins });
   },
 };
