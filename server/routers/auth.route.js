db151dafc444372db2152f4e74310bb13e112752
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-17 11:27:02 +0800

    test route guard

--- server/routers/auth.route.js
+++ server/routers/auth.route.js
@@ -1,6 +1,11 @@
 const UserController = require("../controllers/auth.controller");
 const { catcher } = require("unnamed-js/utils");
+const Admin = require("../models/Admin.model");
 
+const getAdmins = async (req, res) => {
+  // const admins = await Admin.find();
+  req.admins = "admins";
+};
 module.exports = ({ GET, POST, PUT, PATCH, DELETE }) => {
-  GET("/", catcher(UserController.getAllUsers));
+  GET("/", { beforeEnter: [getAdmins] }, catcher(UserController.getAllUsers));
 };
