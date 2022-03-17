de137b57b8cdeceef5dbd918cd4faa631a60591f
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-17 12:16:52 +0800

    add handler

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,7 +2,8 @@ const Admin = require("../models/Admin.model");
 
 module.exports = {
   getAllUsers: async (request, response) => {
-    // const admins = await Admin.findById("65a4d");
+    const admins = await Admin.find();
+    console.log("handler");
     response.send({ admins: "request.admins", admins: request.admins });
   },
 };
