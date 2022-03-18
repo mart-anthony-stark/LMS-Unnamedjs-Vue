ed18dddf9324dfb0e1366dc59bcda56c727c6ec3
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-18 10:48:03 +0800

    add register route

--- server/routers/auth.route.js
+++ server/routers/auth.route.js
@@ -2,15 +2,6 @@ const UserController = require("../controllers/auth.controller");
 const { catcher } = require("unnamed-js/utils");
 const Admin = require("../models/Admin.model");
 
-const getAdmins = async (req, res) => {
-  req.admins = "asd";
-  const admins = await Admin.find();
-  req.admins = admins;
-  console.log("guard");
-};
 module.exports = ({ GET, POST, PUT, PATCH, DELETE }) => {
-  GET("/", { beforeEnter: [getAdmins] }, catcher(UserController.getAllUsers));
-  GET("/ASD", (req, res) => {
-    res.send("ad");
-  });
+  POST("/register", catcher(UserController.register));
 };
