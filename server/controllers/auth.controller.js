64302233c608767a52261a9c888af6ea9eae3047
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-18 10:47:55 +0800

    add register method

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,14 @@
 const Admin = require("../models/Admin.model");
+const { createToken } = require("../utils/token.util");
 
 module.exports = {
-  getAllUsers: async (request, response) => {
-    const admins = await Admin.find();
-    console.log("handler");
-    response.send({ admins: "request.admins", admins: request.admins });
+  register: async (request, response) => {
+    const admin = new Admin(request.body);
+    await admin.save();
+
+    admin._doc.password = undefined;
+
+    const token = createToken({ admin });
+    response.send({ admin: admin._doc, token });
   },
 };
