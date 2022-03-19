98a575b5b3ca656cb616e89997f9756dc4abdf39
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-19 09:52:13 +0800

    add bcrypt

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,12 @@
 const Admin = require("../models/Admin.model");
+const bcrypt = require("bcryptjs");
 const { createToken } = require("../utils/token.util");
 
 module.exports = {
   register: async (request, response) => {
     const admin = new Admin(request.body);
+    const salt = await bcrypt.genSalt(10);
+    admin.password = await bcrypt.hash(request.body.password, salt);
     await admin.save();
 
     admin._doc.password = undefined;
@@ -11,4 +14,8 @@ module.exports = {
     const token = createToken({ admin });
     response.send({ admin: admin._doc, token });
   },
+
+  // login: async (req,res)=>{
+  //   const {email, password} =
+  // }
 };
