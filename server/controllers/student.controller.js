7e2076fb1e05daac6f98977567cfc7aba4397a78
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-20 11:16:21 +0800

    add createStudent method

--- server/controllers/student.controller.js
+++ server/controllers/student.controller.js
@@ -6,7 +6,12 @@ module.exports = {
     res.send(students);
   },
   getOneStudent: async (req, res) => {
-    const student = await Student.findOne({_id: req.params.id});
+    const student = await Student.findOne({ _id: req.params.id });
+    res.send(student);
+  },
+  createStudent: async (req, res) => {
+    const student = new Student(req.body);
+    await student.save();
     res.send(student);
   },
 };
