d86363a30d4f70a8e72f9e18883de76783aae1fc
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-20 11:15:30 +0800

    add createStudent route

--- server/routers/student.route.js
+++ server/routers/student.route.js
@@ -5,6 +5,7 @@ const { catcher } = require("unnamed-js/utils");
 const studentRouter = ({ GET, POST, PUT, PATCH, DELETE }) => {
   GET("/all", catcher(studentController.getAllStudents));
   GET("/:id", catcher(studentController.getOneStudent));
+  POST('/create', catcher(studentController.createStudent))
 };
 
 module.exports = studentRouter;
