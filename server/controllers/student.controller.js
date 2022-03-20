bf382df9a164875c723bd954e02773c80d81f62a
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-20 11:09:08 +0800

    fixed method name getAllStudents

--- server/controllers/student.controller.js
+++ server/controllers/student.controller.js
@@ -1,7 +1,7 @@
 const Student = require("../models/Student.model");
 
 module.exports = {
-  getAllStudent: async (req, res) => {
+  getAllStudents: async (req, res) => {
     const students = await Student.find();
     res.send(students);
   },
