5460a2d0009fdaff05f474c1524aa929ed6c04c7
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-20 11:08:48 +0800

    add get students route

--- server/routers/student.route.js
+++ server/routers/student.route.js
@@ -1,7 +1,7 @@
 const studentController = require('../controllers/student.controller')
 
 const studentRouter = ({GET, POST, PUT, PATCH,DELETE}) => {
-
+    GET('/', studentController.getAllStudents)
 }
 
 module.exports = studentRouter
