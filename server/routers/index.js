93f96ece992d15c786aed611ecd8bccc3f78fa58
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-20 11:04:19 +0800

    registered student route

--- server/routers/index.js
+++ server/routers/index.js
@@ -3,6 +3,10 @@ const routers = [
     prefix: "auth",
     router: require("./auth.route"),
   },
+  {
+    prefix: "student",
+    router: require("./student.route"),
+  },
 ];
 
 module.exports = routers;
