01f8fc1d517e82f313d8cbe2d65be7c1c0578f13
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-20 11:08:37 +0800

    add getAllStudents

--- server/controllers/student.controller.js
+++ server/controllers/student.controller.js
@@ -1,3 +1,8 @@
+const Student = require("../models/Student.model");
+
 module.exports = {
-    
-}
+  getAllStudent: async (req, res) => {
+    const students = await Student.find();
+    res.send(students);
+  },
+};
